51cafa4bcb14726307b3954f979106855988e275
jhchoi <soarnsoar@gmail.com>
2018-10-08 00:22:36 +0200

    Update find_and_change_text.py

--- find_and_change_text.py
+++ find_and_change_text.py
@@ -1,17 +1,47 @@
-filename='test.txt'
-newfilename='newfile.txt'
-old = 'aaa'
-new = 'bbb'
-f = open(filename, 'r')
-newfile = open(newfilename,'w')
+import argparse
+parser = argparse.ArgumentParser()
+####Set options###
+parser.add_argument("--old", help="old phrase")
+parser.add_argument("--new", help="new phrase")
+parser.add_argument("--oldfile", help="old file")
+parser.add_argument("--newfile", help="new file")
+
+args = parser.parse_args()
+
+if args.old:
+    old = args.old
+else:
+    print "need --old argument"
+    exit
+if args.new:
+    new = args.new
+else:
+    print "need --new argument"
+    exit
+if args.oldfile:
+    oldfile= args.oldfile
+else:
+    print "need --oldfile argument"
+    exit
+if args.newfile:
+    newfile= args.newfile
+else:
+    print "need --newfile argument"
+    exit
+
+######################
+
+###Exe####
+f = open(oldfile, 'r')
+f_new = open(newfile,'w')
 lines=f.readlines()
 for line in lines:
     print line
 #    if 'aaa' in line:
     line=line.replace(old,new)
     print line
-    newfile.write(line)
+    f_new.write(line)
 
 
-newfile.close()
+f_new.close()
 f.close()
