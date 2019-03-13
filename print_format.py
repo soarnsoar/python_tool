b8087ecb2ac853d03587921c6a6bd463bbde6538
jhchoi <soarnsoar@gmail.com>
2019-03-13 16:49:08 +0100

    Create print_format.py

---
+++ print_format.py
@@ -0,0 +1,9 @@
+out = open('tmp_Status.html','a')
+
+print>>out,'''                                                                                                                                                                    
+{0}                                                                                                                                                                               
+{1}                                                                                                                                                                               
+{2}                                                                                                                                                                               
+aa                                                                                                                                                                                
+'''.format('1st','2nd','3rd')
+
