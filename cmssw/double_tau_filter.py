b2609887165a8289767d6aa79d98ef789ec33a5f
jhchoi <soarnsoar@gmail.com>
2019-05-07 09:15:29 +0900

    Update double_tau_filter.py

--- cmssw/double_tau_filter.py
+++ cmssw/double_tau_filter.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 lheGenericFilter = cms.EDFilter("LHEGenericFilter",
-    src = cms.InputTag("source"),
+    src = cms.InputTag("externalLHEProducer"),
     NumRequired = cms.int32(2),
     ParticleID = cms.vint32(15),
     AcceptLogic = cms.string("EQ") # LT meaning < NumRequired, GT >, EQ =, NE !=
