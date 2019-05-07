17be0167d163e873a2516a0cd63103eb1e49e0fc
jhchoi <soarnsoar@gmail.com>
2019-05-07 09:50:52 +0900

    Update double_tau_filter.py

--- cmssw/double_tau_filter.py
+++ cmssw/double_tau_filter.py
@@ -1,8 +1,13 @@
+#ref : Hadronizer_TuneCP5_13TeV_EEFilter_LHE_pythia8_cff.py
 import FWCore.ParameterSet.Config as cms
 
-lheGenericFilter = cms.EDFilter("LHEGenericFilter",
+tautauFilter = cms.EDFilter("LHEGenericFilter",
     src = cms.InputTag("source"),
     NumRequired = cms.int32(2),
     ParticleID = cms.vint32(15),
     AcceptLogic = cms.string("EQ") # LT meaning < NumRequired, GT >, EQ =, NE !=
-)                                
+)           
+
+
+ProductionFilterSequence = cms.Sequence(generator+tautauFilter)
+
