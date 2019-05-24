cf4a47745e0749611805f5c3e66c43e444aebf77
junho choi <soarnsoar@gmail.com>
2019-05-24 07:09:17 +0200

    add runtime calc

---
+++ add_runtime.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+INPUT_SCRIPT=sys.argv[1]
+fr=open(INPUT_SCRIPT,'r')
+lines=fr.readlines()
+os.system("mv "+INPUT_SCRIPT+" "+INPUT_SCRIPT+"_old")
+
+
+fnew=open(INPUT_SCRIPT,'w')
+fnew.write('StartTime=$(date +%s)\n')
+
+for line in lines:
+    fnew.write(line)
+
+
+fnew.write('''EndTime=$(date +%s)
+echo $EndTime
+echo "runtime : $(($EndTime - $StartTime)) sec"''')
