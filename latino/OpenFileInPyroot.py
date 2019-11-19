b6e52358646dfee516dc12249e96ac25dda4cafd
your name your last name <soarnsoar@gmail.com>
2019-11-19 16:20:12 +0900

    convert to functional form

--- latino/OpenFileInPyroot.py
+++ latino/OpenFileInPyroot.py
@@ -1,13 +1,14 @@
 from ROOT import TFile,TTree
 import sys
 
-INPUT=sys.argv[1]
-print "INPUT=",INPUT
-if "root://cms-xrdr.private.lo:2094" in INPUT:
-    INPUT=INPUT.split('/xrd/')[1]
-    INPUT='/xrootd/'+INPUT
-    INPUT=INPUT.replace('//','/').replace('/xrootd/store/user/jhchoi/Latino/HWWNano/','/xrootd_user/jhchoi/xrootd/Latino/HWWNano/')
 
+
+def ConvertPath(INPUT):
+    if "root://cms-xrdr.private.lo:2094" in INPUT:
+        INPUT=INPUT.split('/xrd/')[1]
+        INPUT='/xrootd/'+INPUT
+        INPUT=INPUT.replace('//','/').replace('/xrootd/store/user/jhchoi/Latino/HWWNano/','/xrootd_user/jhchoi/xrootd/Latino/HWWNano/')
+    return INPUT
 
 def BoolGetEnties(myTree):
     try:
@@ -18,17 +19,28 @@ def BoolGetEnties(myTree):
         boolean=False
     return boolean
 
-print INPUT
+def OpenFileInPyroot(INPUT):
 
-f=TFile(INPUT,'READ')
-myTree=f.Get("Runs")
-
-
-output=BoolGetEnties(myTree)
-print output
+    print INPUT
+    INPUT=ConvertPath(INPUT)
+    f=TFile(INPUT,'READ')
+    myTree=f.Get("Runs")
 
 
-f.Close()
+    output=BoolGetEnties(myTree)
+    #print output
 
-del myTree
-del f
+
+    f.Close()
+
+    del myTree
+    del f
+    return output
+
+if __name__ == '__main__':
+    INPUT=sys.argv[1]
+    print "INPUT=",INPUT
+
+
+
+    print OpenFileInPyroot(INPUT)
