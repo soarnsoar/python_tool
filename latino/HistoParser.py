2c06620f861bcf3fc14dedfa922868bedab17e35
your name your last name <soarnsoar@gmail.com>
2020-04-24 01:19:28 +0900

    update

--- latino/HistoParser.py
+++ latino/HistoParser.py
@@ -6,7 +6,8 @@ class HistoParser():
         self.mydict=mydict
         ##mydict[grname]={cut,variables,FileName,samples}
         ##will define
-        ##mydict[grname][histo][cut][variables][sample]=TH*D
+        ##mydict[grname]['histo'][cut][variables][sample]=TH*D
+        ##mydict[grname]['histo'][cut][variables]['Sum']=TH*D
         self.ReadHistos()
 
     def ReadHistos(self):
@@ -18,15 +19,26 @@ class HistoParser():
                 self.mydict[gr]['histo'][cut]={}
                 for variable in self.mydict[gr]['variables']:
                     self.mydict[gr]['histo'][cut][variable]={}
+                    idx=0
+                    #self.mydict[gr]['histo'][cut][variable]['Sum']=
                     for sample in self.mydict[gr]['samples']:
                         htemp=f.Get(cut+'/'+variable+'/histo_'+sample)
-                        print "type(htemp)",type(htemp)
+                        #print "type(htemp)",type(htemp)
                         self.mydict[gr]['histo'][cut][variable][sample]=htemp.Clone()
                         self.mydict[gr]['histo'][cut][variable][sample].SetDirectory(0)
+                        if idx==0:
+                            self.mydict[gr]['histo'][cut][variable]['Sum']=htemp.Clone()
+                            self.mydict[gr]['histo'][cut][variable]['Sum'].SetDirectory(0)
+                        else:
+                            self.mydict[gr]['histo'][cut][variable]['Sum'].Add(htemp)
+
                         #print "type(self.mydict[gr]['histo'][cut][variable][sample])",type(self.mydict[gr]['histo'][cut][variable][sample])
                         #print "gr=",gr,'cut=',cut,'variable=',variable,'sample=',sample
                         #print "print type(mydict['gr1']['histo']['eleCH__BoostedggF__SR__METOver40__PtOverM04']['MEKD_Bst_C_0.003_M900']['DATA'])=",type(mydict['gr1']['histo']['eleCH__BoostedggF__SR__METOver40__PtOverM04']['MEKD_Bst_C_0.003_M900']['DATA'])
-            #f.Close()
+                    
+            f.Close()
+            
+
             #print self.mydict[gr]['histo']
         #print type(mydict['gr1']['histo']['eleCH__BoostedggF__SR__METOver40__PtOverM04']['MEKD_Bst_C_0.003_M900']['DATA'])
 if __name__ == '__main__':
@@ -42,6 +54,7 @@ if __name__ == '__main__':
     test=HistoParser(mydict)
     c=ROOT.TCanvas()
     test.mydict['gr1']['histo']['eleCH__BoostedggF__SR__METOver40__PtOverM04']['MEKD_Bst_C_0.003_M900']['DATA'].Draw()
+    test.mydict['gr1']['histo']['eleCH__BoostedggF__SR__METOver40__PtOverM04']['MEKD_Bst_C_0.003_M900']['Sum'].Draw()
     c.SaveAs('test.png')
     
 
