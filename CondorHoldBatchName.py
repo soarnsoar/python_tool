2fd6baaae3f61e79d6b23c1873940701e5b76296
your name your last name <soarnsoar@gmail.com>
2020-07-14 05:57:53 +0900

    add hold functionality

---
+++ CondorHoldBatchName.py
@@ -0,0 +1,16 @@
+import sys
+import os
+#condor_q -global -all -global -all $1 -af:jh Owner RemoteHost CPUsUsage JobPrio JobBatchName|grep jhchoi
+#3772591.0   jhchoi undefined                     undefined             0       NanoGardening__Summer16_102X_nAODv5_Full2016v6__METdo
+def CondorRmBatchName(name):
+
+    command="condor_hold -constraint 'JobBatchName == \""+name+"\"'"
+    os.system(command)
+
+
+
+if __name__ == '__main__':
+    names=sys.argv[1]
+    names=names.split(',')
+    for name in names:
+        CondorRmBatchName(name)
