4796ebdd11853a62df1fad1f6937004546d20695
your name your last name <soarnsoar@gmail.com>
2021-09-29 10:41:40 +0900

    update hadd script for ncpu=1

--- latino/SetupHaddInBatch.py
+++ latino/SetupHaddInBatch.py
@@ -33,7 +33,10 @@ class haddjob():
         self.exe.append("cd "+self.CMSSW_BASE)
         self.exe.append("eval `scramv1 ru -sh`")
         self.exe.append('cd '+self.targetdir)
-        self.exe.append('(mkdir -p temp/;StartTime=$(date +%s);hadd -f -j '+str(self.ncpu)+' -d temp/ hadd.root *.root;EndTime=$(date +%s);echo "runtime : $(($EndTime - $StartTime)) sec";)>hadd.log')
+        if self.ncpu>1:
+            self.exe.append('(mkdir -p temp/;StartTime=$(date +%s);hadd -f -j '+str(self.ncpu)+' -d temp/ hadd.root *.root;EndTime=$(date +%s);echo "runtime : $(($EndTime - $StartTime)) sec";)>hadd.log')
+        else
+            self.exe.append('(mkdir -p temp/;StartTime=$(date +%s);hadd -f hadd.root *.root;EndTime=$(date +%s);echo "runtime : $(($EndTime - $StartTime)) sec";)>hadd.log')
     def SetJds(self):
         self.jds=[]
         self.jds.append('executable = '+self.ExePath)
